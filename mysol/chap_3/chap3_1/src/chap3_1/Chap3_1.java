8606de293b574432472c6f90075a59cd8bbd660f
specter01wj <specter01wj@gmail.com>
2018-10-25 18:18:15 -0400

    first commit

---
+++ mysol/chap_3/chap3_1/src/chap3_1/Chap3_1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chap3_1;
+
+/**
+ *
+ * @author james.wang
+ */
+public class Chap3_1 {
+    static String s1 = "hello";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String s2 = "Java";
+	System.out.println(s1);
+	System.out.println(s2);
+    }
+    
+}
